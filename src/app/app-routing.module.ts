98331d85dd7cf9482d4560a5f9353b567ff82727
James Inglis <jamesekinglis@gmail.com>
2020-03-22 21:29:01 -0600

    created basic dnd component with nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DndComponent } from './dnd/dnd.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  //{ path: 'dnd',      component: DndComponent },
+  { path: 'dnd',      component: DndComponent },
   //{ path: '**', component: HomeComponent }
 ];
 
